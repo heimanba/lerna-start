8dcaa6aabec362bd3258bbc0af39638032b58f53
yuankun <yuankun.yk@alibaba-inc.com>
2021-01-26 16:24:37 +0800

    init

--- packages/a-core/lib/a-core.js
+++ packages/a-core/lib/a-core.js
@@ -6,7 +6,6 @@ module.exports = aCore;
 
 const demo = ["a", "b", "c", "d"];
 _.each(demo, (item) => console.log(item));
-console.log(3344)
 async function aCore() {
   await axios.get("https://api.github.com/users/heimanba");
 }
