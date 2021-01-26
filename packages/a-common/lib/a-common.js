6d06135069154aa54f8cf24b48fd87a83aae8850
yuankun <yuankun.yk@alibaba-inc.com>
2021-01-26 16:22:01 +0800

    init

--- packages/a-common/lib/a-common.js
+++ packages/a-common/lib/a-common.js
@@ -5,6 +5,7 @@ const chalk = require("chalk");
 module.exports = aCommon;
 
 console.log(chalk.red('Hello world!'));
+console.log(222)
 
 async function aCommon() {
   await axios.get("https://api.github.com/users/heimanba");
--- packages/a-core/lib/a-core.js
+++ packages/a-core/lib/a-core.js
@@ -6,7 +6,7 @@ module.exports = aCore;
 
 const demo = ["a", "b", "c", "d"];
 _.each(demo, (item) => console.log(item));
-console.log(222)
+
 async function aCore() {
   await axios.get("https://api.github.com/users/heimanba");
 }
